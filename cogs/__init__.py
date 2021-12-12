0ea9c2b15f65368f23e8ef2d8706d6ab10a7e9a5
Masson <erwann.masson@gmail.com>
2021-12-12 22:57:58 +0100

    Delete files in master

--- cogs/__init__.py
+++
@@ -1,8 +0,0 @@
-# Cogs names linked to their class name
-names = {"admin" : "Admin",
-         "events" : "Events",
-         "usage" : "Usage",
-         "stats" : "Stats",
-         "games" : "Games",
-         "fun" : "Fun"
-}
