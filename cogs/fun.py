14e6a1436274d08ba5ae1ab88bda6ac0286780d6
= <=>
2022-01-24 16:33:31 +0100

    added: say command

--- cogs/fun.py
+++ cogs/fun.py
@@ -9,5 +9,10 @@ class Fun(commands.Cog):
     async def coucou(self, ctx):
         await ctx.send("Coucou !")
 
+    @commands.command(description="répète la phrase donnée",aliases=["repeat","echo"])
+    async def say(self,ctx,*args):
+        await ctx.message.delete()
+        await ctx.send("\""+" ".join(args)+"\"")
+
 def setup(bot):
     bot.add_cog(Fun(bot))
