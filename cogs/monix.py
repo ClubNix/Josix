6e84d66483990afef2a098f5f79e868e2d93d7de
Hitsuji <erwann.mason@gmail.com>
2023-03-12 16:31:23 +0100

    Added check_stocks + leaderboard commands

--- cogs/monix.py
+++ cogs/monix.py
@@ -2,19 +2,21 @@ import discord
 from discord.ext import commands
 from discord import ApplicationContext, option
 
-import requests as r
-import urllib3
-
 from enum import Enum
 from dotenv import load_dotenv
 from os import getenv
+from urllib3 import disable_warnings
+from requests import Session
 from urllib3.exceptions import InsecureRequestWarning
+from dataclasses import dataclass
 
 
-urllib3.disable_warnings(InsecureRequestWarning)
+disable_warnings(InsecureRequestWarning)
 load_dotenv()
+
 JOSIX_LOGIN = getenv("monix_log")
 JOSIX_PSSWD = getenv("monix_psswd") 
+LOG_STOCK = getenv("home") + getenv("logs") + "stocks.txt"
 
 class HTTPMethod(Enum):
     GET = "GET"
@@ -25,12 +27,22 @@ class MonixAPIError(Exception):
     Exception for every Monix API errors
     """
 
+@dataclass(frozen=True)
+class Element:
+    """Class for an element"""
+    name: str
+    value: int
+    isMember: bool
+
+    def __str__(self) -> str:
+        return f"• {self.name} (**{self.value}**" + (" coins)" if self.isMember else ")") +"\n" 
+
 
 class Monix(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         self.base_url = "https://monix.clubnix.fr/api"
-        self.session = r.Session()
+        self.session = Session()
         self.session.verify = False
         
         authentication = self.request(
@@ -56,10 +68,9 @@ class Monix(commands.Cog):
         target: str,
         method: HTTPMethod,
         json: str = None,
-        headers: dict = {},
     ) -> dict:
         """
-        General-purpose function to create web requests to any API created by Anemys.
+        Monix API function to create web requests originally created by Anemys.
         Args:
             target: The endpoint for accessing the ressources we aim for.
             params: dictionary of parameters for the function
@@ -73,7 +84,6 @@ class Monix(commands.Cog):
                 method.value,
                 self.base_url + target,
                 json=json,
-                headers=headers,
             )
         except Exception:
             raise MonixAPIError(f"Unable to connect to {self.base_url}")
@@ -87,6 +97,8 @@ class Monix(commands.Cog):
                 raise MonixAPIError(data.text)
         elif data.status_code == 403:
             raise MonixAPIError("Access denied (403 Forbidden)")
+        elif data.status_code < 200 or data.status_code >= 300:
+            raise MonixAPIError(f"Wrong status code obtained : {data.status_code}")
 
         # Parse the text into JSON
         try:
@@ -117,11 +129,12 @@ class Monix(commands.Cog):
         input_type=int,
         name="count",
         description="The number of products you want to see",
+        min_value=1,
+        max_value=25,
         default=10
     )
     async def see_products(self, ctx: ApplicationContext, count: int):
         await ctx.defer(ephemeral=False, invisible=False)
-
         data = self.request(
             target="/products/",
             method=HTTPMethod.GET
@@ -137,6 +150,159 @@ class Monix(commands.Cog):
 
         await ctx.respond(res)
 
+    @commands.slash_command(description="Check the current stocks and ping the treasurer if they are low")
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    @option(
+        input_type=bool,
+        name="get_stocks",
+        description="Get an output of all the stocks (in .txt file)",
+        default=False
+    )
+    async def check_stocks(self, ctx: ApplicationContext, get_stocks: bool):
+        await ctx.defer(ephemeral=False, invisible=False) 
+        if get_stocks and not ctx.author.guild_permissions.moderate_members:
+            await ctx.respond("You don't have the required permissions to use this parameter")
+            return
+
+        data = self.request(
+            target="/products/",
+            method=HTTPMethod.GET
+        )
+
+        nbStocks = 0
+        if get_stocks:
+            with open(LOG_STOCK, "w") as f:
+                for product in data["data"]:
+                    nbStocks += product["stock"]
+                    f.write(f"{product['name']} : {product['stock']}\n")
+                f.write(f"\n===== Total : {nbStocks} =====\n")
+        
+        else:
+            for product in data["data"]:
+                nbStocks += product["stock"]
+        
+        if nbStocks < 150:
+            roleT = ctx.guild.get_role(1017914272585629788) # Role of the treasurer
+            text = roleT.mention if roleT else "Role not found"
+
+            lowEmbed = discord.Embed(
+                title="Stocks",
+                description=\
+                    f"The stocks are low : **{nbStocks}** remaining\n" \
+                    "You better go shopping or the members will be hungry (and angry) !",
+                color=0xFFCC00
+            )
+            lowEmbed.set_author(name=ctx.author, icon_url=ctx.author.display_avatar)
+            await ctx.respond(content=text, embed=lowEmbed)
+
+        else:
+            highEmbed = discord.Embed(
+                title="Stocks",
+                description=\
+                    f"There is enough stocks : **{nbStocks}** remaining\n" \
+                    "No need to go shopping now !",
+                color=0x1cb82b
+            )
+            highEmbed.set_author(name=ctx.author, icon_url=ctx.author.display_avatar)
+            await ctx.respond(embed=highEmbed)
+
+
+
+    def compareTop(self, top: list[Element], recordVal: int) -> int:
+        if len(top) >= 5:
+            if top[4].value >= recordVal:
+                return -1
+
+            for index, elmt in enumerate(top):
+                if recordVal > elmt.value:
+                    return index
+
+        else:
+            for index, elmt in enumerate(top):
+                if recordVal > elmt.value:
+                    return index
+            return len(top)    
+
+    def compareBottom(self, bottom: list[Element], recordVal: int) -> int:
+        if len(bottom) >= 5:
+            if bottom[4].value <= recordVal:
+                return -1
+
+            for index, elmt in enumerate(bottom):
+                if recordVal < elmt.value:
+                    return index
+
+        else:
+            for index, elmt in enumerate(bottom):
+                if recordVal < elmt.value:
+                    return index
+            return len(bottom)    
+
+
+    @commands.slash_command(description="Leaderboard of the most and least rich members in Monix")
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    @option(
+        input_type=int,
+        name="value_type",
+        description="Which leaderboard you want to see",
+        required=True,
+        choices=[
+            discord.OptionChoice(name="Members", value=0),
+            discord.OptionChoice(name="Products", value=1)
+        ]
+    )
+    async def monix_leaderboard(self, ctx: ApplicationContext, value_type: int):
+        await ctx.defer(ephemeral=False, invisible=False)
+        if value_type != 0 and value_type != 1:
+            await ctx.respond("Unknown value")
+            return 
+
+        data = self.request(
+            target="/users/" if value_type == 0 else "/products",
+            method=HTTPMethod.GET
+        )
+
+        name_type = "members" if value_type == 0 else "products"
+        name_record = "username" if value_type == 0 else "name"
+        name_data = "balance" if value_type == 0 else "stock"
+        top: list[Element] = []
+        bottom: list[Element] = []
+
+        for record in data["data"]:
+            recordVal = record[name_data]
+            recordName = record[name_record]
+            
+            if recordName is None or recordVal is None:
+                continue
+
+            if record["id"] == 1 and value_type == 0:
+                continue
+
+            newElmt = Element(record[name_record], recordVal, value_type==0)
+            checkT = self.compareTop(top, recordVal)
+            if checkT > -1:
+                top.insert(checkT, newElmt)
+                if len(top) > 5:
+                    top.pop()
+
+            # We pass this part for products because a lot of products are at 0 and can't be less
+            if value_type == 1:
+                continue
+
+            checkB = self.compareBottom(bottom, recordVal)
+            if checkB > -1:
+                bottom.insert(checkB, newElmt)
+                if len(bottom) > 5:
+                    bottom.pop()
+                    
+        embed = discord.Embed(
+            title="Leaderboard",
+            color=0x0089FF
+        )
+        embed.set_author(name=ctx.author, icon_url=ctx.author.display_avatar)
+        embed.add_field(name="Top " + name_type, value="".join(map(str, top)))
+        embed.add_field(name="Bottom " + name_type, value="".join(map(str, bottom)))
+        await ctx.respond(embed=embed)
 
 def setup(bot: commands.Bot):
     bot.add_cog(Monix(bot))
